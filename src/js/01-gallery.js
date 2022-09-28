0c50ab5e250ec2d9bb0460820e161ff50ab7ff64
eleza27 <103965241+eleza27@users.noreply.github.com>
2022-09-28 14:53:47 -0400

    final

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery')
+console.log(galleryRef);
+
+const markup = galleryItems.map(item => `
+
+  <a class="gallery__item" href=${item.original}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      alt=${item.description}
+    />
+  </a>
+`)
+    .join('')
+
+galleryRef.insertAdjacentHTML('afterbegin', markup)
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('form');
+const inputRef = document.querySelector('input');
+const textareaRef = document.querySelector('textarea');
+
+const handler = event => {
+  //   localStorage.setItem('email', inputRef.value);
+  //   localStorage.setItem('message', textareaRef.value);
+  const keyData = { email: inputRef.value, message: textareaRef.value };
+  localStorage.setItem('feedback-form-state', JSON.stringify(keyData));
+};
+
+formRef.addEventListener('input', throttle(handler, 500));
+
+const getKeyData = localStorage.getItem('feedback-form-state');
+const lastKeyData = JSON.parse(getKeyData);
+// console.log(JSON.parse(getKeyData));
+
+const onLoadHandler = event => {
+  if (lastKeyData !== null) {
+    inputRef.value = lastKeyData.email;
+    textareaRef.value = lastKeyData.message;
+  }
+};
+addEventListener('load', onLoadHandler);
+
+const onSubmitHandler = event => {
+  event.preventDefault();
+  localStorage.clear();
+  formRef.reset();
+};
+
+formRef.addEventListener('submit', onSubmitHandler);
